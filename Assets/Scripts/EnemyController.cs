87cd577b47566cbbc3471bc10691a261d04f6d68
Rubistyler10 <rubfer04@gmail.com>
2022-11-17 19:36:51 +0100

    LAB Sesion 5 completado
    -Animaciones añadidas
    -Proyectiles añadidos

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,11 +12,14 @@ public class EnemyController : MonoBehaviour
     float timer;
     int direction = 1;
     
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -28,6 +31,8 @@ public class EnemyController : MonoBehaviour
             direction = -direction;
             timer = changeTime;
         }
+    
+    
     }
     
     void FixedUpdate()
@@ -37,10 +42,14 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction;;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction;;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
         
         rigidbody2D.MovePosition(position);
